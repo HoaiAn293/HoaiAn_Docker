f96c0e178ce2b10307ea1f456faa8d416adda9bb
0151_Lê Nguyễn Hoài Ân <lenguyenhoaian293@gmail.com>
2026-03-19 10:48:27 +0700

    init spring boot docker ci

---
+++ src/main/java/com/example/lenguyenhoaian/LenguyenhoaianApplication.java
@@ -0,0 +1,13 @@
+package com.example.lenguyenhoaian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenguyenhoaianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenguyenhoaianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lenguyenhoaian/LenguyenhoaianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lenguyenhoaian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenguyenhoaianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
